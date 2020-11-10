6ac31b684a52cd32a2ff925dd3d78c41575dedae
tujunhui <174640399@qq.com>
2020-11-10 21:53:11 +0800

    fdsafsdfs

--- src/Weixin/Request/WxOpenAddCategory.php
+++ src/Weixin/Request/WxOpenAddCategory.php
@@ -7,24 +7,13 @@ class WxOpenAddCategory extends BaseRequest{
 
     protected $action = '/cgi-bin/wxopen/addcategory';
     protected $method = 'post';
-    protected $data = [
-        'certicates' => [],
-    ];
 
     public function setAccessToken( $access_token ){
         $this->params['access_token'] = $access_token;
     }
 
-    public function setFirst( $first ){
-        $this->data['first'] = $first;
-    }
-
-    public function setSecond( $second ){
-        $this->data['second'] = $second;
-    }
-
-    public function setCerticates( $certicates ){
-        $this->data['certicates'] = $certicates;
+    public function setCategories( $categories ){
+        $this->data['categories'] = $categories;
     }
 
 }
