e84254eb06bebf0d51ec61514a68591c1c37b9ef
tujunhui <174640399@qq.com>
2021-10-01 18:07:43 +0800

    'terdsf'

--- src/Alipay/Requests/AlipayOpenAgentMiniCreateRequest.php
+++ src/Alipay/Requests/AlipayOpenAgentMiniCreateRequest.php
@@ -7,7 +7,7 @@ namespace Mayijuntuan\Alipay\Requests;
  * ALIPAY API: alipay.open.agent.mini.create request
  *
  * @author auto create
- * @since 1.0, 2020-05-07 21:55:55
+ * @since 1.0, 2021-07-13 16:49:34
  */
 class AlipayOpenAgentMiniCreateRequest
 {
@@ -46,6 +46,11 @@ class AlipayOpenAgentMiniCreateRequest
 	 * ISV 代商家操作事务编号，通过事务开启接口alipay.open.agent.create调用返回。
 	 **/
 	private $batchNo;
+	
+	/** 
+	 * 新小程序前台类目，格式为 第一个一级类目_第一个二级类目;第二个一级类目_第二个二级类目_第二个三级类目，详细类目可以通过 https://docs.open.alipay.com/api_49/alipay.open.mini.category.query接口查询mini_category_list。使用后不再读取app_category_ids值，老前台类目将废弃	
+	 **/
+	private $miniCategoryIds;
 	
 	/** 
 	 * 商家小程序客服邮箱
@@ -144,6 +149,17 @@ class AlipayOpenAgentMiniCreateRequest
 	public function getBatchNo()
 	{
 		return $this->batchNo;
+	}
+
+	public function setMiniCategoryIds($miniCategoryIds)
+	{
+		$this->miniCategoryIds = $miniCategoryIds;
+		$this->apiParas["mini_category_ids"] = $miniCategoryIds;
+	}
+
+	public function getMiniCategoryIds()
+	{
+		return $this->miniCategoryIds;
 	}
 
 	public function setServiceEmail($serviceEmail)
