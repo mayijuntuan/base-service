22f72dba3c0771706fef5ff9489cdcf5006d312d
tujunhui <174640399@qq.com>
2022-10-18 22:52:17 +0800

    'tersdfs'

--- src/Weixin/Request/WxaGetTemplateList.php
+++ src/Weixin/Request/WxaGetTemplateList.php
@@ -6,7 +6,6 @@ namespace Mayijuntuan\Weixin\Request;
 class WxaGetTemplateList extends BaseRequest{
 
     protected $action = '/wxa/gettemplatelist';
-    protected $method = 'post';
 
     public function setTemplateType( $template_type ){
         $this->data['template_type'] = $template_type;
